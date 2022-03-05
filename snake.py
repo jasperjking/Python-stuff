8999077f88a6a501d67f8c2527346523d67f79b6
Jasper King <jjvking@icloud.com>
2022-03-05 14:16:07 +1100

    khjl

--- snake.py
+++ snake.py
@@ -1,17 +1,31 @@
 import pygame
+pygame.init()
+
+WIDTH, HEIGHT = 700, 750
+WIN = pygame.display.set_mode((WIDTH, HEIGHT))
+FPS = 60
+
+GREY = (40, 40, 40)
 
 def draw_window():
-    pass
+    WIN.fill(GREY)
+    
+    pygame.display.update()
 
 def main():
     
     run = True
+    clock = pygame.time.Clock()
     while run:
-        for event in pygame.event.get():
-            if event.type == pygame.quit():
-                run = False
+        clock.tick(FPS)
 
-    pygame.quit()
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+                pygame.quit()
+
+        draw_window()
+
 
 if __name__ == "__main__":
     main()
